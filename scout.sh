4049b59c195d989bebda28dd99af030e7b191a74
akilhylton <hyltonakil@gmail.com>
2021-05-10 18:23:14 -0400

    Added support for masscan

--- scout.sh
+++ scout.sh
@@ -6,26 +6,32 @@ AMASS_BIN="https://github.com/OWASP/Amass/releases/download/v3.12.3/$AMASS_VER"
 GOWITNESS_DIR="/usr/local/bin/gowitness"
 GOWITNESS_VER="gowitness-2.3.4-linux-amd64"
 GOWITNESS_BIN="https://github.com/sensepost/gowitness/releases/download/2.3.4/$GOWITNESS_VER"
+MASSCAN_DIR="/usr/bin/masscan"
 
 install_tools() {
 amass() {
         if [ ! -f $AMASS_DIR ]; then
-                wget $AMASS_BIN && unzip $AMASS_VER && mv amass_linux_amd64/amass /usr/local/bin/ 
+                wget $AMASS_BIN && unzip $AMASS_VER && mv amass_linux_amd64/amass $AMASS_DIR 
         fi
 	echo "Amass is installed and located at $AMASS_DIR"
 }
-
 gowitness() {
         if [ ! -f $GOWITNESS_DIR ]; then
                 wget $GOWITNESS_BIN && chmod +x $GOWITNESS_VER && mv $GOWITNESS_VER $GOWITNESS_DIR
         fi
 	echo "Gowitness is installed and located at $GOWITNESS_DIR"
 }
-
-amass && gowitness
+masscan() {
+	if [ ! -f $MASSCAN_DIR ]; then
+		sudo apt install masscan
+	fi
+        echo "Masscan is installed and located at $MASSCAN_DIR"
+}
+amass && gowitness && masscan
 }
 
 clean() {
 	ls | grep -e "^\a" | xargs rm -rf
 }
+
 install_tools && clean
